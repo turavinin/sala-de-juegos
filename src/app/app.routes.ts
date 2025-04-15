457e0c0e48a9c45005bb60fe899a0a0e2100df1b
turavinin <aturavinin@gmail.com>
2025-04-15 18:58:45 -0300

    first sprint

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'about', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'about', component: AboutComponent },
+    { path: '**', component: AboutComponent }
+];
