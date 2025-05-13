88beed8635e0c8065191d11bee9aec89f3cadceb
turavinin <aturavinin@gmail.com>
2025-05-13 19:03:21 -0300

    .

---
+++ src/app/components/survey/survey.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-survey',
+  imports: [],
+  templateUrl: './survey.component.html',
+  styleUrl: './survey.component.scss'
+})
+export class SurveyComponent {
+
+}
