db2e633053b81f3e6a7832fa3a03000b3732ad5d
turavinin <aturavinin@gmail.com>
2025-05-20 19:03:53 -0300

    .

--- src/app/modules/games/games.module.ts
+++ src/app/modules/games/games.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { GamesRoutingModule } from './games-routing.module';
 import { PreguntadosGameComponent } from '../../components/games/preguntados-game/preguntados-game.component';
+import { TragamonedasGameComponent } from '../../components/games/tragamonedas-game/tragamonedas-game.component';
+import { MayormenorGameComponent } from '../../components/games/mayormenor-game/mayormenor-game.component';
+import { AhoracadoGameComponent } from '../../components/games/ahoracado-game/ahoracado-game.component';
 
 
 @NgModule({
@@ -10,7 +13,10 @@ import { PreguntadosGameComponent } from '../../components/games/preguntados-gam
   imports: [
     CommonModule,
     GamesRoutingModule,
-    PreguntadosGameComponent
+    PreguntadosGameComponent,
+    TragamonedasGameComponent,
+    MayormenorGameComponent,
+    AhoracadoGameComponent
   ]
 })
 export class GamesModule { }
